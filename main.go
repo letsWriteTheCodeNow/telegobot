ed6bb177a0f83a3832481902359ad2677a618bd4
letsWriteTheCodeNow <89580133+letsWriteTheCodeNow@users.noreply.github.com>
2021-08-27 10:49:08 +0300

    ф

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln(err)
+	}
+	teleToken := os.Getenv("teleToken")
+	url := "https://api.telegram.org/bot" + teleToken + "/getUpdates" + "?timeout=300"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	log.Println(string(body))
+
+	// https: //api.telegram.org/bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11/getMe
+
+}
